7eb877a89c9c48eef93a2a472dfa7f569b3d79b2
Aditya Khandelwal <31183279+ak149@users.noreply.github.com>
2020-10-26 11:29:37 +0530

    string

---
+++ Introductory Problems/13_Create Strings I.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF  						1e15
+#define EPS   						1e-9
+#define MOD 						1000000007
+#define Pi 							3.14159265358979323846264338327950288419716939937510
+
+#define umap 						unordered_map
+#define uset 						unordered_set
+#define fi 							first
+#define se 							second
+#define ll 							long long 
+#define vi 							vector<int>
+#define pii 						pair<int,int>
+#define vll 						vector<ll>
+#define pll							pair<ll,ll>
+#define pb 							push_back
+#define pob 						pop_back
+#define pf 							push_front
+#define pof 						pop_front
+#define maxE(a,n) 					*max_element(a,a+n);
+#define minE(a,n) 					*min_element(a,a+n);
+#define all(x) 						x.begin(), x.end()
+#define rev(x)						reverse(all(x))
+
+#define fast 						ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fori(i,a,n) 				for (ll  i=a;i<n;i++)
+#define ford(i,a,n) 				for (ll  i=n-1;i>=a;i--)
+
+ll gcd(ll a, ll b){	return __gcd(a,b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll power(ll x, ll b,ll m) {ll p = 1;while (b > 0) {if(b&1) {p = p * x; p %= m;} b >>= 1; x *= x; x %= m;} return p % m;}
+void dbg(string s){cout<<s<<endl;}
+
+
+void solve(){
+
+	string s;
+	cin>>s;
+	sort(all(s));
+	int count = 0;
+	vector<string> op;
+	do{
+		op.pb(s);
+		count++;
+	}
+	while(next_permutation(s.begin(),s.end()));
+	cout<<count<<endl;
+	for(string s:op){
+		cout<<s<<endl;
+	}
+	return;
+
+}
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("D:\\CODING\\CSES\\input.txt", "r", stdin);
+	freopen("D:\\CODING\\CSES\\output.txt", "w", stdout);
+	#endif
+
+	fast;
+	ll t = 1;
+
+	while(t--){
+		solve();
+	}
+}
