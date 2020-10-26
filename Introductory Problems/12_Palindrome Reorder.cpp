d8df2d37c09f54b8453f53d5dd357bf777df473d
Aditya Khandelwal <31183279+ak149@users.noreply.github.com>
2020-10-26 11:12:14 +0530

    palindrome add

--- Introductory Problems/12_Palindrome Reorder.cpp
+++ Introductory Problems/12_Palindrome Reorder.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 #define INF  						1e15
 #define EPS   						1e-9
 #define MOD 						1000000007
 #define Pi 							3.14159265358979323846264338327950288419716939937510
-
+ 
 #define umap 						unordered_map
 #define uset 						unordered_set
 #define fi 							first
@@ -22,96 +22,64 @@ using namespace std;
 #define maxE(a,n) 					*max_element(a,a+n);
 #define minE(a,n) 					*min_element(a,a+n);
 #define all(x) 						x.begin(), x.end()
-
+ 
 #define fast 						ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define fori(i,a,n) 				for (ll  i=a;i<n;i++)
 #define ford(i,a,n) 				for (ll  i=n-1;i>=a;i--)
-
+ 
 ll gcd(ll a, ll b){ll temp;while (b > 0){temp = a%b;a = b;b = temp;} return a;}
 ll lcm(ll a, ll b){return a*b/gcd(a,b);}
 ll power(ll x, ll b,ll m) {ll p = 1;while (b > 0) {if(b&1) {p = p * x; p %= m;} b >>= 1; x *= x; x %= m;} return p % m;}
 void dbg(string s){cout<<s<<endl;}
-
-
+ 
+ 
 void solve(){
-
+ 
 	string s;
 	cin>>s;
 	// cout<<s<<endl;
-	vector<ll> frealpha(26,0);
+	map<char,string> seen;
 	for(auto ch:s){
-		frealpha[ch - 'A']++;
-	}
-
-	int countodd = 0;
-	for(auto fre:frealpha){
-		if(fre%2 == 1){
-			countodd++;
+		if(seen.find(ch) !=  seen.end()){
+			seen[ch] += ch;
+		}
+		else{
+			seen[ch] = ch;
 		}
 	}
-	// cout<<countodd<<endl;
-	if(countodd > 1){
-		cout<<"NO SOLUTION"<<endl;
-		return;
-	}
-	else{
-		string op = "";
-		int oddchar = INT_MAX;
-		for(int i = 0 ;i < 26 ;i++){
-			if(frealpha[i]&1)
-				oddchar = i;
+
+	string st = "",end = "",odd = "";
+
+	for(auto p: seen){
+		if( (int)p.se.size() & 1 && odd != ""){
+			cout<<"NO SOLUTION"<<endl;
+			return;
 		}
-
-		for(int i = 0 ;i < 26;i++){
-			if(frealpha[i]&1)
-				continue;
-			int j = frealpha[i]/2;
-			frealpha[i] /= 2;
-			char ch = 'A' + i;
-			while(j--){
-				op = op + ch;
-			}
+		else if( (int)p.se.size() & 1 ){
+			odd = p.se;
 		}
-		char oddch = '\0';
-		if(oddchar != INT_MAX){
-			char oddch = 'A' + oddchar;
-			while(frealpha[oddchar]--){
-				op = op + oddch;
-			}
+		else{
+			st = st + p.se.substr(0,p.se.size()/2);
+			end  = p.se.substr(0,p.se.size()/2) + end;
 		}
-
-		// for(int i = 0; i < 26 ; i++){
-		// 	char ch = 'A' + i;
-
-		// 	cout<<"fre of"<<ch<<" : "<<frealpha[i]<<endl;
-		// }
-
-		for(int i = 25 ;i >= 0 ;i--){
-			char ch = 'A' + i;
-			if(ch == oddch)
-				continue;
-			int j = frealpha[i];
-			while(j--){
-				op = op + ch;
-			}
-		}
-
-		cout<<op<<endl;
 	}
 
+	string op = "";
+	op = st + odd + end;
+	cout<<op<<endl;
 }
-
-
+ 
+ 
 int main(){
-
+ 
 	#ifndef ONLINE_JUDGE
 	freopen("D:\\CODING\\CSES\\input.txt", "r", stdin);
 	freopen("D:\\CODING\\CSES\\output.txt", "w", stdout);
 	#endif
-
+ 
 	fast;
 	ll t = 1;
-
+ 
 	while(t--){
 		solve();
 	}
