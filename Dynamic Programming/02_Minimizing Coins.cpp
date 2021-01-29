724d61dcee983b1d40c4121472755b0e25b37ab3
Aditya Khandelwal <31183279+ak149@users.noreply.github.com>
2021-01-29 16:33:55 +0530

    dp2

---
+++ Dynamic Programming/02_Minimizing Coins.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF  						1e15
+#define EPS   						1e-9
+#define MOD 						1000000007
+#define Pi 							3.14159265358979323846264338327950288419716939937510
+
+#define umap 						unordered_map
+#define uset 						unordered_set
+#define fi 							first
+#define se 							second
+#define ll 							long long 
+#define vi 							vector<int>
+#define pii 						pair<int,int>
+#define vll 						vector<ll>
+#define pll							pair<ll,ll>
+#define vvll						vector<vector<ll>>
+#define pb 							push_back
+#define pob 						pop_back
+#define pf 							push_front
+#define pof 						pop_front
+#define maxE(a,n) 					*max_element(a,a+n);
+#define minE(a,n) 					*min_element(a,a+n);
+#define all(x) 						x.begin(), x.end()
+#define rev(x)						reverse(all(x))
+
+#define fast 						ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i,n)					for (ll  i=0;i<n;i++)
+#define repd(i,n)					for (ll  i = n-1;i>=0;i--)
+#define fori(i,a,n) 				for (ll  i=a;i<n;i++)
+#define ford(i,a,n) 				for (ll  i=n-1;i>=a;i--)
+#define trav(x,a)					for(auto x:a){cout<<x<<" ";}
+	
+ll gcd(ll a, ll b){	return __gcd(a,b);}
+ll lcm(ll a, ll b){return (a*b)/gcd(a,b);}
+ll power(ll x, ll b,ll m) {ll p = 1;while (b > 0) {if(b&1) {p = p * x; p %= m;} b >>= 1; x *= x; x %= m;} return p % m;}
+void dbg(string s){cout<<s<<endl;}
+
+
+void solve(){
+
+	ll n,x;
+	cin>>n>>x;
+
+	vector<ll> coins(n);
+	rep(i,n)	cin>>coins[i];
+
+	vector<ll> dp(x + 1,INT_MAX);
+	dp[0] = 0;
+	fori(i,1,x + 1){
+		rep(j,n){
+			if(i >= coins[j]){
+				dp[i] = min(dp[i],1 + dp[i - coins[j]]);
+			}
+		}
+	}
+
+	if(dp[x] == INT_MAX){
+		cout<<-1<<endl;
+	}
+	else{
+		cout<<dp[x]<<endl;
+	}
+
+	
+
+}
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	fast;
+	ll t = 1;
+
+	while(t--){
+		solve();
+	}
+}
