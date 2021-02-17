e7652941e631d880530bd60f0744df5cb42139a9
Aditya Khandelwal <31183279+ak149@users.noreply.github.com>
2021-02-18 01:32:23 +0530

    changed name

---
+++ Sorting and Searching/02_Appartements.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF  						1e15
+#define EPS   						1e-9
+#define MOD 						1000000007
+#define Pi 							3.14159265358979323846264338327950288419716939937510
+
+#define umap 						unordered_map
+#define uset 						unordered_set
+#define fi 							first
+#define se 							second
+#define ll 							long long 
+#define vi 							vector<int>
+#define pii 						pair<int,int>
+#define vll 						vector<ll>
+#define pll							pair<ll,ll>
+#define vvll						vector<vector<ll>>
+#define pb 							push_back
+#define pob 						pop_back
+#define pf 							push_front
+#define pof 						pop_front
+#define maxE(a,n) 					*max_element(a,a+n);
+#define minE(a,n) 					*min_element(a,a+n);
+#define all(x) 						x.begin(), x.end()
+#define rev(x)						reverse(all(x))
+#define endl						"\n"
+
+#define fast 						ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i,n)					for (ll  i=0;i<n;i++)
+#define repd(i,n)					for (ll  i = n-1;i>=0;i--)
+#define fori(i,a,n) 				for (ll  i=a;i<n;i++)
+#define ford(i,a,n) 				for (ll  i=n-1;i>=a;i--)
+#define trav(x,a)					for(auto x:a){cout<<x<<" ";}
+	
+ll gcd(ll a, ll b){	return __gcd(a,b);}
+ll lcm(ll a, ll b){return (a*b)/gcd(a,b);}
+ll power(ll x, ll b,ll m) {ll p = 1;while (b > 0) {if(b&1) {p = p * x; p %= m;} b >>= 1; x *= x; x %= m;} return p % m;}
+void dbg(string s){cout<<s<<endl;}
+
+
+/*PBDS*/
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+typedef tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> PBDS;
+
+// pbds.find_by_order(k) (return iterator to kth largest element(from 0))
+// pbds.order_of_key(k) (return number of elements in set which are strictly less than k)
+
+void solve(){
+
+	ll n,m,k;
+	cin>>n>>m>>k;
+
+	vector<ll> d(n);
+	rep(i,n)	cin>>d[i];
+
+	vector<ll> a(m);
+	rep(i,m)	cin>>a[i];
+
+	sort(all(a));
+	sort(all(d));
+
+	int nsold = 0;
+	int i = 0;
+	int j = 0;
+
+	while(i < n && j < m ){
+		if(abs(a[j] - d[i]) <= k){
+			nsold++;
+			i++;
+			j++;
+		}
+
+		else if(a[j] > d[i]){
+			i++;
+		}
+
+		else{
+			j++;
+		}
+	}
+
+	cout<<nsold<<endl;
+
+
+}
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	fast;
+	ll t = 1;
+
+	while(t--){
+		solve();
+	}
+}
