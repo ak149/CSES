b45246df908542efc0b5e7349b3156a5257c5d7e
Aditya Khandelwal <31183279+ak149@users.noreply.github.com>
2021-09-06 14:37:31 +0530

    ss5

---
+++ Sorting and Searching/05_Restaurant Customers.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF                          1e15
+#define EPS                           1e-9
+#define MOD                         1000000007
+#define Pi                             3.14159265358979323846264338327950288419716939937510
+
+#define umap                         unordered_map
+#define uset                         unordered_set
+#define fi                             first
+#define se                             second
+#define ll                             long long
+#define vi                             vector<int>
+#define pii                         pair<int,int>
+#define vll                         vector<ll>
+#define pll                            pair<ll,ll>
+#define vvll                        vector<vector<ll>>
+#define pb                             push_back
+#define pob                         pop_back
+#define pf                             push_front
+#define pof                         pop_front
+#define maxE(a,n)                     *max_element(a,a+n);
+#define minE(a,n)                     *min_element(a,a+n);
+#define all(x)                         x.begin(), x.end()
+#define rev(x)                        reverse(all(x))
+#define endl                        "\n"
+
+#define fast                         ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i,n)                    for (ll  i=0;i<n;i++)
+#define repd(i,n)                    for (ll  i = n-1;i>=0;i--)
+#define fori(i,a,n)                 for (ll  i=a;i<n;i++)
+#define ford(i,a,n)                 for (ll  i=n-1;i>=a;i--)
+#define trav(x,a)                    for(auto x:a){cout<<x<<" ";}cout<<endl;
+
+ll gcd(ll a, ll b) {    return __gcd(a, b);}
+ll lcm(ll a, ll b) {return (a * b) / gcd(a, b);}
+ll power(ll x, ll b, ll m) {ll p = 1; while (b > 0) {if (b & 1) {p = p * x; p %= m;} b >>= 1; x *= x; x %= m;} return p % m;}
+void dbg(string s) {cout << s << endl;}
+
+
+/*PBDS*/
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> PBDS;
+
+// pbds.find_by_order(k) (return iterator to kth largest element(from 0))
+// pbds.order_of_key(k) (return number of elements in set which are strictly less than k)
+
+void solve() {
+
+    ll n;
+    cin >> n;
+
+    vector<ll> arr(n), dep(n);
+    rep(i, n)    cin >> arr[i] >> dep[i];
+
+    sort(all(arr));
+    sort(all(dep));
+
+    ll cnt = 1;
+    ll maxcnt = 1;
+    ll timer = arr[0];
+
+    ll i = 1;
+    ll j = 0;
+
+    while (i < n && j < n) {
+        if (arr[i] <= dep[j]) {
+            cnt++;
+            maxcnt = max(maxcnt, cnt);
+            timer = arr[i];
+            i++;
+        }
+        else {
+            cnt--;
+            j++;
+        }
+    }
+
+
+    cout << maxcnt << endl;
+}
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    fast;
+    ll t = 1;
+
+    while (t--) {
+        solve();
+    }
+}
